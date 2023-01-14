25c6246ae95f2e07714fe6b4c521de68bc3848dc
bostonsinaga <aganisautiluramnotsob@gmail.com>
2023-01-14 08:19:56 +0700

    ultimate design

--- script.js
+++ script.js
@@ -33,19 +33,19 @@ let projectFrame_img_arr = ['', ''];
 // KML TOWN
 for (let i = 0; i < 8; i++) {
     projectFrame_img_arr[0] += (
-        `<img src="${projectFrame_filedir_arr[0][i]}" alt="kml-town-screenshot" style="opacity: 0%"/>`
+        `<img src="${projectFrame_filedir_arr[0][i]}" alt="kml-town-screenshot" style="display: none" class="img-normal"/>`
     );
 
     if (i < 5) {
         projectFrame_img_arr[1] += (
-            `<img src="${projectFrame_filedir_arr[1][i]}" alt="callapet-screenshot" style="opacity: 0%"/>`
+            `<img src="${projectFrame_filedir_arr[1][i]}" alt="callapet-screenshot" style="display: none" class="img-normal"/>`
         );
     }
 }
 
 const projectTitle_str_arr = [
-    `<a href="https://github.com/bostonsinaga/KML-TOWN" target="_blank">KML-TOWN</a> (2022)`,
-    `<a href="https://play.google.com/store/apps/dev?id=7043024713824441776" target="_blank">CALLAPET</a> (2020-2021)`
+    `<a href="https://github.com/bostonsinaga/KML-TOWN">KML-TOWN</a> (2022)`,
+    `<a href="https://play.google.com/store/apps/dev?id=7043024713824441776">CALLAPET</a> (2020-2021)`
 ];
 
 const projectDescription_str_arr = [
@@ -62,11 +62,11 @@ const projectDescription_str_arr = [
     /* CALLAPET */
 
     "Android games I ever made that have released on Google Play Store. Sequentially named " +
-    "<a href=\"https://play.google.com/store/apps/details?id=com.callapet.callamice\" target=\"_blank\">'Call a Mice'</a>, " +
-    "<a href=\"https://play.google.com/store/apps/details?id=com.callapet.flyingpinball\" target=\"_blank\">'Flying Pinball'</a>, " +
-    "<a href=\"https://play.google.com/store/apps/details?id=com.callapet.zombiehelicoptergap\" target=\"_blank\">'Zombie Helicopter Gap'</a>, " +
-    "<a href=\"https://play.google.com/store/apps/details?id=com.callapet.flapduobirds\" target=\"_blank\">'Flap Duo Birds'</a>, " +
-    "<a href=\"https://play.google.com/store/apps/details?id=com.callapet.tilesmathjumper\" target=\"_blank\">'Tiles Math Jumper'</a>. " +
+    "<a href=\"https://play.google.com/store/apps/details?id=com.callapet.callamice\">'Call a Mice'</a>, " +
+    "<a href=\"https://play.google.com/store/apps/details?id=com.callapet.flyingpinball\">'Flying Pinball'</a>, " +
+    "<a href=\"https://play.google.com/store/apps/details?id=com.callapet.zombiehelicoptergap\">'Zombie Helicopter Gap'</a>, " +
+    "<a href=\"https://play.google.com/store/apps/details?id=com.callapet.flapduobirds\">'Flap Duo Birds'</a>, " +
+    "<a href=\"https://play.google.com/store/apps/details?id=com.callapet.tilesmathjumper\">'Tiles Math Jumper'</a>. " +
     "I used Cocos2d-x the open source game engine with C++. The game apps source codes are open on my GitHub. " +
     "Creating games was the reason for me to write code. I have learned by myself from watching video tutorials, read articles and online forums."
 ];
@@ -75,9 +75,11 @@ for (let i = 0; i < 2; i++) {
     section_projects_DOM.innerHTML += `
         <div class="section-projects-board">
             <div class="section-projects-board-flag" style="display: none">1</div>
-            <div class="section-projects-board-frames">${projectFrame_img_arr[i]}</div>
-            <div class="section-projects-board-left-button"></div>
-            <div class="section-projects-board-right-button"></div>
+            <a class="section-projects-board-frames section-projects-board-frames-normal">
+                <div class="section-projects-board-frames-left-button section-projects-board-frames-left-button-normal"></div>
+                <div class="section-projects-board-frames-right-button section-projects-board-frames-right-button-normal"></div>
+                ${projectFrame_img_arr[i]}
+            </a>
             <div class="section-projects-board-about">
                 <h1 class="section-projects-board-about-title">${projectTitle_str_arr[i]}</h1>
                 <div class="section-projects-board-about-description">${projectDescription_str_arr[i]}</div>
@@ -90,11 +92,12 @@ for (let i = 0; i < 2; i++) {
 
 const clickArrow = (arrowNode, valSign) => {
 
-    const flagNode = arrowNode.parentNode.querySelector(".section-projects-board-flag");
-    const framesImgNodes = arrowNode.parentNode.querySelectorAll(".section-projects-board-frames img");
+    // arrow -> frames -> board
+    const flagNode = arrowNode.parentNode.parentNode.querySelector(".section-projects-board-flag");
+    const framesImgNodes = arrowNode.parentNode.parentNode.querySelectorAll(".section-projects-board-frames img");
 
     let curDex = parseInt(flagNode.innerText);
-    framesImgNodes[curDex].style.opacity = '0%';
+    framesImgNodes[curDex].style.display = "none";
     curDex += valSign;
 
     if (curDex < 0 && valSign < 0) {
@@ -104,21 +107,194 @@ const clickArrow = (arrowNode, valSign) => {
         curDex = 0;
     }
 
-    framesImgNodes[curDex].style.opacity = '100%';
-    flagNode.innerText = curDex;    
+    framesImgNodes[curDex].style.display = "block";
+    flagNode.innerText = curDex;
 }
 
-const leftArrowNodes = document.querySelectorAll(".section-projects-board-left-button");
-const rightArrowNodes = document.querySelectorAll(".section-projects-board-right-button");
+const leftArrowNodes = document.querySelectorAll(".section-projects-board-frames-left-button");
+const rightArrowNodes = document.querySelectorAll(".section-projects-board-frames-right-button");
 
 // init frames visibilty
 clickArrow(leftArrowNodes[0], -1); // kml-town
 clickArrow(leftArrowNodes[1], -1); // callapet
 
 for (let leftEl of leftArrowNodes) {
-    leftEl.addEventListener("click", () => {clickArrow(leftEl, -1);});
+    leftEl.addEventListener("click", (event) => {
+        event.stopPropagation();
+        event.preventDefault();
+        clickArrow(leftEl, -1);
+    });
 }
 
 for (let rightEl of rightArrowNodes) {
-    rightEl.addEventListener("click", () => {clickArrow(rightEl, 1);});
+    rightEl.addEventListener("click", (event) => {
+        event.stopPropagation();
+        event.preventDefault();
+        clickArrow(rightEl, 1);
+    });
 }
+
+function setBoardFrameLink(frameNode, isInsert) {
+
+    let boardFramesCtr = 0;
+    let boardFramesImg_links = [
+        "https://github.com/bostonsinaga/KML-TOWN",
+        "https://play.google.com/store/apps/dev?id=7043024713824441776"
+    ];
+
+    if (frameNode) {
+        if (isInsert) {
+            for (let testNode of document.querySelectorAll(".section-projects-board-frames")) {
+                if (testNode == frameNode) {
+                    testNode.href = boardFramesImg_links[boardFramesCtr];
+                    break;
+                }
+                boardFramesCtr++;
+            }
+        }
+        else frameNode.removeAttribute("href");
+    }
+    else {
+        if (isInsert) {
+            for (let node of document.querySelectorAll(".section-projects-board-frames")) {
+                node.href = boardFramesImg_links[boardFramesCtr];
+                boardFramesCtr++;
+            }
+        }
+        else {
+            for (let node of document.querySelectorAll(".section-projects-board-frames")) {
+                node.removeAttribute("href");
+            }
+        }
+    }
+}
+
+let isEnlarged = false;
+
+// click board frames to enlarge images
+for (let frameNode of document.querySelectorAll(".section-projects-board-frames")) {
+    frameNode.addEventListener("click", (event) => {
+        
+        if (window.innerWidth >= 900) {
+            event.stopPropagation();
+
+            if (!isEnlarged) {
+                document.querySelector(".dark-bg-off").classList.add("dark-bg-on");
+
+                frameNode.classList.remove("section-projects-board-frames-normal");
+                frameNode.classList.add("section-projects-board-frames-enlarge");
+
+                for (let imgNode of frameNode.querySelectorAll("img")) {
+                    imgNode.classList.remove("img-normal");
+                    imgNode.classList.add("img-enlarge");
+                }
+
+                isEnlarged = true;
+
+                // left arrow //
+
+                frameNode.parentNode.querySelector(".section-projects-board-frames-left-button")
+                    .classList.remove("section-projects-board-frames-left-button-normal");
+
+                frameNode.parentNode.querySelector(".section-projects-board-frames-left-button")
+                    .classList.add("section-projects-board-frames-left-button-enlarge");
+
+                // right arrow //
+
+                frameNode.parentNode.querySelector(".section-projects-board-frames-right-button")
+                    .classList.remove("section-projects-board-frames-right-button-normal");
+
+                frameNode.parentNode.querySelector(".section-projects-board-frames-right-button")
+                    .classList.add("section-projects-board-frames-right-button-enlarge");
+
+                // hide about
+                frameNode.parentNode.querySelector(".section-projects-board-about")
+                    .classList.add("section-projects-board-about-enlarge");
+
+                setTimeout(() => setBoardFrameLink(frameNode, true), 0.1);
+            }
+        }
+    });
+}
+
+function defaultAppearance() {
+    if (isEnlarged) {
+        for (let frameNode of document.querySelectorAll(".section-projects-board-frames")) {
+            document.querySelector(".dark-bg-off").classList.remove("dark-bg-on");
+
+            frameNode.classList.remove("section-projects-board-frames-enlarge");
+            frameNode.classList.add("section-projects-board-frames-normal");
+
+            for (let imgNode of frameNode.querySelectorAll("img")) {
+                imgNode.classList.remove("img-enlarge");
+                imgNode.classList.add("img-normal");
+            }
+
+            isEnlarged = false;
+
+            // left arrow //
+
+            frameNode.parentNode.querySelector(".section-projects-board-frames-left-button")
+                .classList.remove("section-projects-board-frames-left-button-enlarge");
+
+            frameNode.parentNode.querySelector(".section-projects-board-frames-left-button")
+                .classList.add("section-projects-board-frames-left-button-normal");
+
+            // right arrow //
+
+            frameNode.parentNode.querySelector(".section-projects-board-frames-right-button")
+                .classList.remove("section-projects-board-frames-right-button-enlarge");
+
+            frameNode.parentNode.querySelector(".section-projects-board-frames-right-button")
+                .classList.add("section-projects-board-frames-right-button-normal");
+
+            // show about
+            frameNode.parentNode.querySelector(".section-projects-board-about")
+                .classList.remove("section-projects-board-about-enlarge");
+
+            setTimeout(() => setBoardFrameLink(frameNode, false), 0.1);
+        }
+    }
+}
+
+// enlarged to default
+const universe_DOM = document.querySelector(".UNIVERSE");
+universe_DOM.addEventListener("click", () => defaultAppearance());
+document.body.addEventListener("keydown", (e) => {
+    if (e.key == "Escape") defaultAppearance();
+});
+
+// on small screen (portrait) //
+
+const header_DOM = universe_DOM.querySelector("header"),
+      header_about_DOM = header_DOM.querySelector(".header-about"),
+      header_figure_DOM = header_DOM.querySelector(".header-figure");
+
+let isSmallScreenDetected_1050 = false,
+    isSmallScreenDetected_900 = false;
+
+setInterval(() => {
+    if (window.innerWidth <= 1050) {
+        if (!isSmallScreenDetected_1050) {
+            isSmallScreenDetected_1050 = true;
+            header_DOM.removeChild(header_about_DOM);
+            header_figure_DOM.insertBefore(header_about_DOM, header_figure_DOM.firstChild);
+        }
+
+        if (window.innerWidth <= 900) {
+            if (!isSmallScreenDetected_900) {
+                isSmallScreenDetected_900 = true;
+                setBoardFrameLink(undefined, true);
+            }
+        }
+        else if (isSmallScreenDetected_900) {
+            isSmallScreenDetected_900 = false;
+            setBoardFrameLink(undefined, false);
+        }
+    }
+    else if (isSmallScreenDetected_1050) {
+        isSmallScreenDetected_1050 = false;
+        header_figure_DOM.removeChild(header_about_DOM);
+        header_DOM.insertBefore(header_about_DOM, header_DOM.firstChild);
+    }
+}, 0.1);
