503a7d3aaa7c1f735d56702afced74d3778ab3c3
bostonsinaga <bostonsinga@gmail.com>
2025-10-24 09:25:35 +0700

    Add 3 class 'DirectoryNode', 'FileNode', and 'ExecutableNode'. Not completed yet but will they will related as inheritance
    Separate static line creation into reusable 'createStaticLine' function

--- script.js
+++ script.js
@@ -1,35 +1,156 @@
 /**
- * This class represents a file path.
- * The last node without a next node is the current directory.
+ * 
  */
-class CLINode {
+function createStaticLine(promptString, commandString) {
+  // create static line
+  const staticLine_DOM = document.createElement('div');
+  staticLine_DOM.classList.add('static');
+  staticLine_DOM.classList.add('line');
+  staticLine_DOM.contentEditable = 'false';
+
+  // create static prompt
+  const staticPrompt_DOM = document.createElement('div');
+  staticPrompt_DOM.classList.add('prompt');
+  staticPrompt_DOM.textContent = promptString;
+
+  // create static command
+  const staticCommand_DOM = document.createElement('div');
+  staticCommand_DOM.classList.add('command');
+  staticCommand_DOM.textContent = commandString;
+
+  // put static line above newline
+  staticLine_DOM.appendChild(staticPrompt_DOM);
+  staticLine_DOM.appendChild(staticCommand_DOM);
+
+  return staticLine_DOM;
+}
+
+/**
+ * 
+ */
+class ExecutableNode {
+  constructor(name, extension, callback) {
+
+    if (typeof name === 'string') {
+      this.name = name;
+    }
+
+    if (typeof extension === 'string') {
+      this.extension = extension;
+    }
+
+    if (typeof callback === 'function') {
+      this.callback = callback;
+    }
+    else this.callback = () => {};
+  }
+
+  execute(testStr) {
+    if (testStr === this.name ||
+      testStr === this.name + '.' + this.extension
+    ) {
+      this.callback();
+      return staticContent_DOM;
+    }
+
+    return null;
+  }
+};
+
+class FileNode {
+  constructor(name, extension, content, content) {
+    if (typeof name === 'string') {
+      this.name = name;
+    }
+
+    if (typeof extension === 'string') {
+      this.extension = extension;
+    }
+
+    if (typeof extension === 'string') {
+      this.content = content;
+    }
+  }
+
+  read(testStr) {
+    if (testStr === this.name ||
+      testStr === this.name + '.' + this.extension
+    ) {
+      const staticContent_DOM = document.createElement('div');
+      staticContent_DOM.classList.add('static');
+      staticContent_DOM.classList.add('line');
+      staticContent_DOM.contentEditable = 'false';
+      staticContent_DOM.textContent = this.content;
+
+      return staticContent_DOM;
+    }
+
+    return null;
+  }
+};
+
+const cd_exenode = new ExecutableNode(
+  'cd', '', () => {
+    // if 
+  }
+);
+
+/**
+ * This class represents a directory.
+ * This is a doubly linked list.
+ */
+class DirectoryNode {
   constructor(name) {
     this.name = name;
+    this.prev = null;
     this.next = null;
+    this.exenodes = [];
+  }
+
+  setPrev(prev) {
+    this.prev = prev;
   }
 
   setNext(next) {
     this.next = next;
   }
 
-  stringify() {
+  addExecutableNode(exenode) {
+    if (exenode instanceof ExecutableNode) {
+      this.exenodes.push(exenode);
+    }
+  }
+
+  stringifyPath() {
     let current = this;
     let text = '';
 
+    // previous nodes
     while (current.next) {
       text += current.name;
       current = current.next + '\\';
     }
 
+    // current node
     text += current.name + '>';
     return text;
   }
 };
 
-// create node started from root
-let node = new CLINode('home');
-
+// Boston Sinaga CEO Siberasta
+// Boston Sinaga CEO Otometro
+// Boston Sinaga CEO Jelkasa
+/**  */
 document.addEventListener('DOMContentLoaded', () => {
+
+  // create node started from root
+  let dirnode = new DirectoryNode('home');
+
+  dirnode.addExecutableNode(
+    new ExecutableNode('foo', 'exe', 'Hello World!')
+  );
+
+  const rootDirnode = dirnode;
 
   // access DOMs
   const cli_DOM = document.getElementById('cli');
@@ -39,7 +160,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // prompt-command initialization
   dynamicCommand_DOM.focus();
-  dynamicPrompt_DOM.innerText = node.stringify();
+  dynamicPrompt_DOM.innerText = dirnode.stringifyPath();
 
   // cursor always active and placed at the end of newline
   cli_DOM.addEventListener('click', () => {
@@ -61,26 +182,21 @@ document.addEventListener('DOMContentLoaded', () => {
       // get string from user input
       const text = dynamicCommand_DOM.textContent.trim();
 
-      // create static line
-      const staticLine_DOM = document.createElement('div');
-      staticLine_DOM.classList.add('static');
-      staticLine_DOM.classList.add('line');
-      staticLine_DOM.contentEditable = 'false';
+      // 
+      cli_DOM.insertBefore(
+        createStaticLine(dirnode.stringifyPath(), text),
+        dynamicLine_DOM
+      );
 
-      // create static prompt
-      const staticPrompt_DOM = document.createElement('div');
-      staticPrompt_DOM.classList.add('prompt');
-      staticPrompt_DOM.textContent = node.stringify();
+      // 
+      for (const exe of dirnode.exenodes) {
+        const staticContent_DOM = exe.execute(text);
 
-      // create static command
-      const staticCommand_DOM = document.createElement('div');
-      staticCommand_DOM.classList.add('command');
-      staticCommand_DOM.textContent = text;
-
-      // put static line above newline
-      staticLine_DOM.appendChild(staticPrompt_DOM);
-      staticLine_DOM.appendChild(staticCommand_DOM);
-      cli_DOM.insertBefore(staticLine_DOM, dynamicLine_DOM);
+        if (staticContent_DOM) {
+          cli_DOM.insertBefore(staticContent_DOM, dynamicLine_DOM);
+          break;
+        }
+      }
 
       // keep focus to input
       dynamicCommand_DOM.textContent = '';
