efaef767153b7fb7ba33188703f410faf6843ffa
bostonsinaga <bostonsinga@gmail.com>
2025-10-27 16:34:25 +0700

    Add 'charIsNumber' and 'stringIsNumber' functions

--- src/regex.js
+++ src/regex.js
@@ -1,5 +1,5 @@
 /**
- * Checks if the string is all dot characters.
+ * Checks whether the string is all dot characters.
  * @param {string} str - test string
  * @returns {boolean}
  */
@@ -15,4 +15,22 @@ export function isStringAllDots(str) {
  */
 export function isIllegalCharacterFilename(str) {
   return /[!"#$%&*:<>/\\?|]/.test(str);
+}
+
+/**
+ * Checks whether a character is number.
+ * @param {string} ch - test character
+ * @returns {boolean}
+ */
+export function charIsNumber(ch) {
+  return /^[0-9]$/.test(ch);
+}
+
+/**
+ * Checks whether all the characters are number.
+ * @param {string} str - test string
+ * @returns {boolean}
+ */
+export function stringIsNumber(str) {
+  return /^[0-9]+$/.test(str);
 }
